a249b2a8b26fc903e577b87829d9d062daf691cc
Iago Passos <iago.dpassos@gmail.com>
2026-08-22 17:32:55 -0300

    Bump version to 0.4.0

--- app/main.cpp
+++ app/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	QApplication::setApplicationName(QStringLiteral("LibreMerge"));
-	QApplication::setApplicationVersion(QStringLiteral("0.3.0"));
+	QApplication::setApplicationVersion(QStringLiteral("0.4.0"));
 	QApplication::setOrganizationName(QStringLiteral("LibreMerge"));
 
 	// translations follow the system language (LIBREMERGE_LANGUAGE
