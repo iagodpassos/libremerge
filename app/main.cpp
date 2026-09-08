388e90e677cc6683fc1e7d86ff02443de685bf6f
Iago Passos <iago.dpassos@gmail.com>
2026-09-08 20:11:54 -0300

    Bump version to 0.9.4

--- app/main.cpp
+++ app/main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	QGuiApplication::setDesktopFileName(QStringLiteral("libremerge"));
 	QApplication::setApplicationName(QStringLiteral("LibreMerge"));
-	QApplication::setApplicationVersion(QStringLiteral("0.9.3"));
+	QApplication::setApplicationVersion(QStringLiteral("0.9.4"));
 	QApplication::setOrganizationName(QStringLiteral("LibreMerge"));
 
 	// translations follow the system language unless overridden by the
